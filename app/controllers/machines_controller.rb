f688d9737c7ea3ed76b0fabfe99395c2cc65750d
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-08-14 00:08:38 -0700

    machine list view, machine creation

---
+++ app/controllers/machines_controller.rb
@@ -0,0 +1,25 @@
+class MachinesController < ApplicationController
+	def index
+		@machines = Machine.all
+	end
+
+	def new
+		@machine = Machine.new
+	end
+
+	def create
+		p params
+		@machine = Machine.new(machine_params)
+		if @machine.save
+			redirect_to machines_path
+		else
+			render :new
+			puts "Issue with machine creation"
+		end
+	end
+
+	private
+  def machine_params
+    params.require(:machine).permit(:serial_num, :customer_id)
+  end
+end
