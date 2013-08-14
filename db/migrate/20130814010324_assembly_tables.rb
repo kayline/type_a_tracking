7b9dd6f39d4282b33d0b3b7ce9c0a794215a5367
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-08-13 19:07:47 -0700

    initial migration for machines, assembly processes and tasks

--- db/migrate/20130814010324_assembly_tables.rb
+++ db/migrate/20130814010324_assembly_tables.rb
@@ -1,4 +1,33 @@
 class AssemblyTables < ActiveRecord::Migration
   def change
+	create_table :machines do |t|
+		t.integer :serial_num
+		t.references :customer
+		t.timestamps
+	end
+
+	create_table :assembly_procs do |t|
+			t.references :machine
+		t.date :begin_date, :end_date
+		t.boolean :complete, default: false
+		t.timestamps
+		end
+
+		create_table :subassem_procs do |t|
+			t.references :assembly_proc
+			t.string :name
+			t.integer :stage
+			t.boolean :complete, default: false
+			t.timestamps
+		end
+
+		create_table :assem_tasks do |t|
+			t.references :subassem_proc
+			t.string :title
+			t.string :description
+			t.boolean :complete, default: false
+			t.timestamps
+		end
+
   end
 end
