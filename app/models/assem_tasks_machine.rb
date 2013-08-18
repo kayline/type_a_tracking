b70f090a66cc72a62377557d71e1bc71f8dadf2e
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-08-17 22:08:17 -0700

    easier acces to parent task title and description through task instance

--- app/models/assem_tasks_machine.rb
+++ app/models/assem_tasks_machine.rb
@@ -2,4 +2,8 @@ class AssemTasksMachine < ActiveRecord::Base
 	belongs_to :machine 
 	belongs_to :assem_task
 	delegate :subassembly, :to => :assem_task
+	delegate :title, :to => :assem_task
+	delegate :description, :to => :assem_task
+
+
 end
